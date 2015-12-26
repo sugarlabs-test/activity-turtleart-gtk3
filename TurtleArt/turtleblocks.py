88252ad7c4bfc3edd8e33a5a5bf2d6fd2a4c9a07
Cristian García <cristian99garcia@gmail.com>
2015-12-26 10:22:07 -0300

    Fixing small bug

--- TurtleArt/turtleblocks.py
+++ TurtleArt/turtleblocks.py
@@ -39,6 +39,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
+from gi.repository import GdkPixbuf
 
 try:
     # Try to use XDG Base Directory standard for config files.
